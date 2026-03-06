522be6921cbc3aa372c0d9b8b2c575b65e8639c1
Volha Bolatava <vola_bolatava@epam.com>
2026-03-06 16:43:41 +0400

    --session_1 task1-task3

--- Python-Course/Session_1/module_1.py
+++ Python-Course/Session_1/module_1.py
@@ -2,25 +2,36 @@ from typing import List
 
 
 def task_1(array: List[int], target: int) -> List[int]:
-    """
-    Write your code below
-    """
-    pass
-
+    result= set()
+    for num in array:
+        dif=target-num
+        if dif in result:
+            return[dif,num]
+        result.add(num)
+    return[]
 
 def task_2(number: int) -> int:
-    """
-    Write your code below
-    """
-    pass
+    reversed=0
+    negative = number < 0
 
+    number = abs(number)
+
+    while number > 0:
+        digit = number % 10
+        reversed = reversed * 10 + digit
+        number = number // 10
+
+    if negative:
+        reversed=-reversed
+    return reversed
 
 def task_3(array: List[int]) -> int:
-    """
-    Write your code below
-    """
-    pass
-
+    for i in range(len(array)):
+        index = abs(array[i])-1
+        if array[index]<0:
+            return abs(array[i])
+        array[index]=-array[index]
+    return -1
 
 def task_4(string: str) -> int:
     """
